35eadd1cec6713dd36fddeb8b7f918985c20ca79
tbeaugureau809 <tbeaugureau809@gmail.com>
2025-10-28 19:53:30 +0200

    Section4 complete

---
+++ Section4/s4challenge/challenge.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    
+    
+    std::cout<<"What is your favorite number between 1 and 100?"<<std::endl;
+
+    int fav_number;
+    std::cin >> fav_number;
+
+    std::cout<< "Amazing!! That's my favorite number too!"<<std::endl;
+    std:: cout<<"No really!!, "<<fav_number<<" is my favorite number!"<<std::endl;
+
+    return 0;
+
+}
