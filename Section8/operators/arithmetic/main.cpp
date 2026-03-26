6b9475951650ec8321a6824654af721d6474bef3
tbeaugureau809 <tbeaugureau809@gmail.com>
2026-03-26 07:01:53 +0200

    New Sections

---
+++ Section8/operators/arithmetic/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1 {200};
+    int num2 {100};
+
+    //cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
+
+    int result{0};
+
+    result = num1
+}
---
+++ Section8/operators/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int num1 {10};
+    int num2 {20};
+ 
+    num1 = 50;
+    cout<< "num1 is " <<num1 <<endl;
+    cout<< "num2 is " <<num2 <<endl;
+    
+    cout<<endl;
+    return 0;
+
+}
